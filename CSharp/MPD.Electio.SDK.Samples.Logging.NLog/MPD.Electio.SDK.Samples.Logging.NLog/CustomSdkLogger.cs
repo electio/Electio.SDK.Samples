9387d8ba30a82b00c0c064f8a8f2e2ac367accb5
mrose-mpd <michael.rose@mpd-group.com>
2016-08-15 11:00:15 +0100

    Added sample logging implementation
    Added a new class library that demonstrates how to implement the Electio
    SDK ILogger interface using NLog - this is simplest example possible.

---
+++ CSharp/MPD.Electio.SDK.Samples.Logging.NLog/MPD.Electio.SDK.Samples.Logging.NLog/CustomSdkLogger.cs
@@ -0,0 +1,46 @@
+﻿using NLog;
+using ILogger = MPD.Electio.SDK.Interfaces.ILogger;
+
+namespace MPD.Electio.SDK.Samples.Logging.NLog
+{
+    public class CustomSdkLogger : ILogger
+    {
+        private readonly Logger _logger;
+
+        public CustomSdkLogger()
+        {
+            const string APP_NAME = "MyCustomAppName"; //supply this however you choose
+            _logger = LogManager.GetLogger(APP_NAME);
+        }
+
+        public void Debug(string format, params object[] args)
+        {
+            _logger.Log(LogLevel.Debug, format, args);
+        }
+
+        public void Info(string format, params object[] args)
+        {
+            _logger.Log(LogLevel.Info, format, args);
+        }
+
+        public void Trace(string format, params object[] args)
+        {
+            _logger.Log(LogLevel.Trace, format, args);
+        }
+
+        public void Warn(string format, params object[] args)
+        {
+            _logger.Log(LogLevel.Warn, format, args);
+        }
+
+        public void Error(string format, params object[] args)
+        {
+            _logger.Log(LogLevel.Error, format, args);
+        }
+
+        public void Fatal(string format, params object[] args)
+        {
+            _logger.Log(LogLevel.Fatal, format, args);
+        }
+    }
+}
---
+++ CSharp/MPD.Electio.SDK.Samples.Logging.NLog/MPD.Electio.SDK.Samples.Logging.NLog/readme.md
@@ -0,0 +1,7 @@
+﻿#Electio SDK Logging
+
+##Overview
+
+This is a sample library that demonstrates how to integrate the built-in Electio SDK logging into your application.
+
+This sample uses (NLog)[http://nlog-project.org/] but you can easily adapt the sample for your own preferred logging library.
